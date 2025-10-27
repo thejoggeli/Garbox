b96babeb445c8cf203dfb1fc843da5874e1baaf4
test <test>
2025-10-27 23:29:01 +0100

    color map vector based

--- src/color/ColorMap.h
+++ src/color/ColorMap.h
@@ -1,18 +1,24 @@
 #pragma once
 
-#include <array>
+#include <vector>
 #include <cstddef>
 #include "RgbFloat.h"
 #include "HslColor.h"
 
 namespace Garbox {
 
+/*
+ * WARNING:
+ * This class performs dynamic heap allocation.
+ * It is intended for use only as a static or global object in embedded environments.
+ * Do NOT instantiate it on the stack or in frequently called functions.
+ */
 class ColorMap {
 public:
     ColorMap(std::initializer_list<RgbFloat> colors);
     ColorMap(std::initializer_list<HslColor> colors);
 
-    ~ColorMap();
+    ~ColorMap() = default;
 
     ColorMap(const ColorMap&) = delete;
     ColorMap& operator=(const ColorMap&) = delete;
@@ -22,12 +28,11 @@ public:
     RgbFloat interpolateRgb(float t) const;
     HslColor interpolateHsl(float t) const;
 
-    size_t size() const { return entryCount; }
+    size_t size() const { return rgbMap.size(); }
 
 private:
-    RgbFloat* rgbMap;
-    HslColor* hslMap;
-    size_t entryCount;
+    std::vector<RgbFloat> rgbMap;
+    std::vector<HslColor> hslMap;
 };
 
-} // namespace
+} // namespace Garbox
